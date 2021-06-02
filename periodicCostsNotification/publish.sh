de1bb8846322b6231cb48feac8a77e6244e7e85d
MatteoGioioso <gioioso.matteo@gmail.com>
2021-06-02 20:16:28 +0700

    feat(): add SAR publishing

---
+++ periodicCostsNotification/publish.sh
@@ -0,0 +1,17 @@
+export AWS_REGION=ap-southeast-1
+export VERSION=1.0.0
+export STACK_NAME=periodic-costs-notification
+export BUCKET=${STACK_NAME}-lambda-artifacts
+
+# make the deployment bucket in case it doesn't exist
+aws s3 mb s3://"${BUCKET}"
+
+aws cloudformation package \
+  --template-file template.yaml \
+  --output-template-file output.yaml \
+  --s3-bucket "${BUCKET}"
+
+sam publish \
+    --template output.yaml \
+    --region "${AWS_REGION}" \
+    --semantic-version "${VERSION}"
